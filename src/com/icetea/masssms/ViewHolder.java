452c3c66a0fd4ffca6efafdc3a5f00352ae34975
xingren <xingren_work@163.com>
2014-03-11 01:11:20 +0800

    Delete ViewHolder.java

--- src/com/icetea/masssms/ViewHolder.java
+++
@@ -1,9 +0,0 @@
-package com.icetea.masssms;
-
-import android.widget.Checkable;
-import android.widget.TextView;
-
-public class ViewHolder {
-	public TextView textView;
-	public Checkable checkable;
-}
